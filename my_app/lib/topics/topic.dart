28ec7dc06f65f51b8abea637b62bd7568aa94bf5
Andrew Park <ajpark5@uci.edu>
2022-10-16 23:40:47 -0700

    Added firestore topic retrieval + basic card view

---
+++ my_app/lib/topics/topic.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/services/models.dart';
+import 'package:my_app/topics/topics.dart';
+// import 'package:my_app/shared/progress_bar.dart';
+
+class TopicEntry extends StatelessWidget {
+  final Topic topic;
+  const TopicEntry({super.key, required this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: topic.thumbnail,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => TopicScreen(topic: topic),
+              ),
+            );
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 3,
+                child: SizedBox(
+                  child: Image.asset(
+                    'assets/thumbnails/${topic.thumbnail}',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Text(
+                    topic.title,
+                    style: const TextStyle(
+                      height: 1.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopicScreen extends StatelessWidget {
+  final Topic topic;
+  const TopicScreen({super.key, required this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: ListView(children: [
+        Hero(
+          tag: topic.thumbnail,
+          child: Image.asset('assets/thumbnails/${topic.thumbnail}',
+              width: MediaQuery.of(context).size.width),
+        ),
+        Text(
+          topic.title,
+          style: const TextStyle(
+              height: 2, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ]),
+    );
+  }
+}
--- my_app/lib/topics/topics.dart
+++ my_app/lib/topics/topics.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/shared/bottom_nav.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_app/services/services.dart';
+import 'package:my_app/shared/shared.dart';
+import 'package:my_app/topics/topic.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
-    );
+    return FutureBuilder(
+        future: FirestoreService().getTopics(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingScreen();
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            List<Topic> topics = snapshot.data as List<Topic>;
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.blue,
+                title: const Text('Topics'),
+              ),
+              body: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20.0),
+                crossAxisSpacing: 10.0,
+                crossAxisCount: 1,
+                children:
+                    topics.map((topic) => TopicEntry(topic: topic)).toList(),
+              ),
+              bottomNavigationBar: const BottomNavBar(),
+            );
+          } else {
+            return const Text('There ar no topics at the moment...');
+          }
+        });
   }
 }
